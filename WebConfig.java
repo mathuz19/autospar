165f17e49b8366bc100a66c2a43f175e2facfd7c
IT24104291 <IT24104291@my.sliit.lk>
2025-05-26 16:50:54 +0530

    Add files via upload

---
+++ WebConfig.java
@@ -0,0 +1,17 @@
+package com.example.demo.config;
+
+import org.springframework.lang.NonNull; // Use Spring's NonNull annotation
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(@NonNull CorsRegistry registry) { // Use @NonNull from Spring
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+}
